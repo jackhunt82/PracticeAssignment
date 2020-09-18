1953b2f02a1484662e215def831b0a755bfda839
jackh <jackh@DESKTOP-1MQDR5N>
2020-09-18 12:09:08 -0500

    Version 2 PracticeAssignment

--- PracticeAssignment/Program.cs
+++ PracticeAssignment/Program.cs
@@ -6,7 +6,10 @@ namespace PracticeAssignment
     {
         static void Main(string[] args)
         {
+            // Pause to display message
+            Console.WriteLine("Press any key to continue...");
             Console.WriteLine("Hello World!");
+            Console.ReadKey(true);
         }
     }
 }
